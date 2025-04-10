9d1a6e10382e8d08131aee929d16ff7c8d39484f
Bruno Lopes <brunniel.lopes@gmail.com>
2025-04-10 03:24:00 +0100

    fix: fixed setup and improved the config files to a better aproach

--- laravel/docker-entrypoint.sh
+++ laravel/docker-entrypoint.sh
@@ -1,28 +1,36 @@
 #!/bin/bash
 set +x
-cd /var/www/
+cd /var/www/html/
 
-if [[ ! "$(ls -A "/var/www/")" ]] ;
+if [[ ! "$(ls -A "/var/www/html/")" ]] ;
 then
     composer create-project --prefer-dist laravel/laravel:^{LARAVEL_VERSION}.0 .
 fi
 
-if [[ -f "/var/www/composer.json" ]] ;
+if [[ -f "/var/www/html/composer.json" ]] ;
 then
-    if [[ ! -d "/var/www/vendor" ]] ;
+    if [[ ! -d "/var/www/html/vendor" ]] ;
     then
         composer install
     fi
 fi
 
-if [[ ! -f ".env" ]] ;
+if [[ ! -f "/var/www/html/.env" ]] ;
 then
     echo ".env file not found"
-    cp .env.example .env
-    php artisan key:generate
+    if [[ -f "/var/www/html/.env.example" ]] ;
+    then
+        cp /var/www/html/.env.example /var/www/html/.env
+        php artisan key:generate
+    else
+        echo ".env.example file not found"
+    fi
 fi
 
-cp /usr/local/apache2/conf/httpd.conf.bk /usr/local/apache2/conf/httpd.conf
-sudo chown -R apache:apache /var/www 2> /dev/null
+cp /etc/apache2/sites-available/000-default.conf.bk /etc/apache2/sites-available/000-default.conf
+mkdir -p /var/www/html/storage
+mkdir -p /var/www/html/bootstrap/cache
+chown -R www-data:www-data /var/www/html /var/www/html/storage /var/www/html/bootstrap/cache 2> /dev/null
+chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache 2> /dev/null
 
 exec "$@"
